436d8c37bf423a7093826b33e0179b2233d7911e
essejose <ze_pk2@hotmail.com>
2018-01-03 23:39:45 -0200

    game estrutura

---
+++ Assets/Scripts/BolaScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BolaScript : MonoBehaviour {
+ 
+    public int vel;
+    public int velMax;
+
+    private Rigidbody2D myBody;
+
+    // Use this for initialization
+    void Start () {
+        velMax = 5;
+        myBody = this.GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter2D(Collision2D target)
+    {
+
+        if (target.gameObject.tag == "bloco")
+        {
+            Destroy(target.gameObject);
+        }
+
+       if (target.gameObject.tag == "plataforma")  
+        {
+
+            // myBody.AddForce(new Vector2(vel * Random.Range(-10, 10), 300));
+            myBody.AddForce(new Vector2(vel * Random.Range(-10, 10), 300));
+
+            if (vel < velMax)
+            {
+
+                vel = vel + 1;
+
+            }
+
+        }
+    }
+
+}
---
+++ Assets/Scripts/ControlePlataformaScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlePlataformaScript : MonoBehaviour {
+
+
+    public float limiteEsquerdo, limiteDireito;
+    public float velocidade;
+  
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Mover();
+       
+    }
+
+    void Mover()
+    {
+        float move_x = Input.GetAxisRaw("Horizontal") * velocidade * Time.deltaTime;
+
+        transform.Translate(move_x, 0, 0.0f);
+       
+
+    }
+
+}
